581134fcf4baf3446c033b27800d3101f9111a69
Mykhailo Hriha <mykhailho.hriha@gmail.com>
2020-08-18 13:20:46 +0300

    start

---
+++ src/main/java/com/hriha/FloorLayout/FloorLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.hriha.FloorLayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloorLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hriha/FloorLayout/FloorLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hriha.FloorLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
